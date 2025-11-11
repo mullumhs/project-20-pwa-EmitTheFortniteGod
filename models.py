1e3e47a35224beafa02c1509d3d54483329d2cd9
EmitTheFortniteGod <147779921+EmitTheFortniteGod@users.noreply.github.com>
2025-11-12 10:38:05 +1100

    Made Data Base Model

--- models.py
+++ models.py
@@ -1,6 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime
+
+
+
 db = SQLAlchemy()
 
-# Define your database model here
-# Example: class Item(db.Model):
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)        # Its Nessasary
+    title = db.Column(db.String(100), nullable=False)   # Drink name 
+    type = db.Column(db.String(50), nullable=False)     # "beer", "wine", "spirit"
+    brand = db.Column(db.String(100))                   # Coopers, Jim Beans , WolfBlast
+    sweetness = db.Column(db.Integer)                   # 1–10 scale
+    percentage = db.Column(db.Float)                    # ABV %
+
+    def __repr__(self):
+
+        return f'<Task {self.title}>'
